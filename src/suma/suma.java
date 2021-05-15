c61246f2b39d682f9a1d9a17e7ae5e74eb35e1f0
madelem <mmadelemnayra@gmail.com>
2021-05-14 20:45:17 -0400

    subiendo proyecto suma

---
+++ src/suma/suma.java
@@ -0,0 +1,15 @@
+package suma;
+
+public class suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,r ;
+		a=5;
+		b=2;
+		r=a+b;
+		System.out.println(r);
+
+	}
+
+}
